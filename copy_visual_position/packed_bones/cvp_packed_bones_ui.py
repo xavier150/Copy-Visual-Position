5b41ae38059cddafdd24009c034224768ed16ed4
BleuRaven <Xavierloux.loux@gmail.com>
2025-08-17 08:17:26 +0200

    Update python starting file lisence and infos.

--- copy_visual_position/packed_bones/cvp_packed_bones_ui.py
+++ copy_visual_position/packed_bones/cvp_packed_bones_ui.py
@@ -1,20 +1,11 @@
-# ====================== BEGIN GPL LICENSE BLOCK ============================
+# SPDX-FileCopyrightText: 2018-2025 Xavier Loux (BleuRaven)
 #
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#  All rights reserved.
-#
-# ======================= END GPL LICENSE BLOCK =============================
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# ----------------------------------------------
+#  Copy Visual Position
+#  https://github.com/xavier150/Copy-Visual-Position
+# ----------------------------------------------
 
 # ----------------------------------------------
 #  with this Addons you will be able to easily copy and paste the visual position of an object in your scene, this works also for Vertices in EditMode and also the chains of Bones in PoseMode
